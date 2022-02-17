ef338c26c28b646d336adfe52f6d6d04ff7e947e
Claire Farrow <clairefarrow.cp@gmail.com>
2022-02-17 11:23:01 +0000

    resolved issues following merge

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -91,34 +91,37 @@ describe("/api/articles/", () => {
           expect(articles).toBeSortedBy("created_at", {
             descending: true,
           });
+        })
+    })
+
 
         
-          test('Status 200: Articles are sorted-by date in descending order', () => {
-            return request(app)
-            .get("/api/articles")
-            .expect(200)
-            .then(({ body: { articles } }) => {
-                expect(articles).toBeSortedBy("created_at", {
-                    descending: true
-                })
-            })
-          })
+          
+    test('Status 200: Articles are sorted-by date in descending order', () => {     
+      return request(app)   
+      .get("/api/articles")    
+      .expect(200)     
+      .then(({ body: { articles } }) => {      
+        expect(articles).toBeSortedBy("created_at", {          
+          descending: true       
+        })      
+      })   
+    })
       
-          test('Status: 200 - Returns an array of comments for the given article_id', () => {
-            const id = 3
-            return request(app).get(`/api/articles/${id}/comments`).expect(200).then(({ body: { comments } }) => {
-              expect(comments).toEqual(expect.objectContaining({
-                article_id: id,
-                comment_id: expect.any(Number),
-                votes: expect.any(Number),
-                created_at: expect.any(String),
-                author: expect.any(String),
-                body: expect.any(String)
-              }))
-            })
-          })
-        });
-    });
+          
+    test('Status: 200 - Returns an array of comments for the given article_id', () => {   
+      const id = 3   
+      return request(app).get(`/api/articles/${id}/comments`).expect(200).then(({ body: { comments } }) => {     
+        expect(comments).toEqual(expect.objectContaining({     
+          article_id: id,   
+          comment_id: expect.any(Number),  
+          votes: expect.any(Number),   
+          created_at: expect.any(String),     
+          author: expect.any(String),     
+          body: expect.any(String)
+        }))
+      })
+    })
 
     test("Status: 404 - responds with error msg for VALID but Non-existent article_id", () => {
       return request(app)
@@ -138,79 +141,84 @@ describe("/api/articles/", () => {
     });
   });
 
-  describe("PATCH", () => {
-    test("Status: 200 - accept an object of inc_votes which increments vote by a positive number", () => {
-      const positiveVote = { inc_votes: 1 };
-      return request(app)
-        .patch(`/api/articles/2`)
-        .send(positiveVote)
-        .expect(200)
-        .then(({ body }) => {
-          expect(body.article).toEqual(
-            expect.objectContaining({
-              article_id: 2,
-              title: expect.any(String),
-              topic: "mitch",
-              author: "icellusedkars",
-              body: expect.any(String),
-              created_at: expect.any(String),
-              votes: 1,
-            })
-          );
-        });
-    });
-    test("Status: 200 - accept an object of inc_votes which decrements vote by a negative number", () => {
-      const negativeVote = { inc_votes: -100 };
-      return request(app)
-        .patch(`/api/articles/4`)
-        .send(negativeVote)
-        .expect(200)
-        .then(({ body }) => {
-          expect(body.article).toEqual(
-            expect.objectContaining({
-              article_id: 4,
-              title: expect.any(String),
-              topic: "mitch",
-              author: "rogersop",
-              body: expect.any(String),
-              created_at: expect.any(String),
-              votes: -100,
-            })
-          );
-        });
-    });
-    test("status: 400 - reponse with err msg for invalid inc_vote request", () => {
-      const invalidVote = { inc_votes: NaN };
-      return request(app)
-        .patch("/api/articles/3")
-        .send(invalidVote)
-        .expect(400)
-        .then(({ body: { msg } }) => {
-          expect(msg).toBe("Bad Request");
-        });
-    });
-    test("status: 400 - reponse with err msg inc_vote spelt wrong but with VALID number", () => {
-      const invalidVote = { inc_voes: 3 };
-      return request(app)
-        .patch("/api/articles/3")
-        .send(invalidVote)
-        .expect(400)
-        .then(({ body: { msg } }) => {
-          expect(msg).toBe("Bad Request");
-        });
-    });
-    test("status: 400 - reponse with err msg when extra key is added to the inc_vote request", () => {
-      const invalidVote = { inc_votes: 3, title: "invalid" };
-      return request(app)
-        .patch("/api/articles/3")
-        .send(invalidVote)
-        .expect(400)
-        .then(({ body: { msg } }) => {
-          expect(msg).toBe("Bad Request");
-        });
+
+    
+  
+    describe("PATCH", () => {
+      test("Status: 200 - accept an object of inc_votes which increments vote by a positive number", () => {
+        const positiveVote = { inc_votes: 1 };
+        return request(app)
+          .patch(`/api/articles/2`)
+          .send(positiveVote)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.article).toEqual(
+              expect.objectContaining({
+                article_id: 2,
+                title: expect.any(String),
+                topic: "mitch",
+                author: "icellusedkars",
+                body: expect.any(String),
+                created_at: expect.any(String),
+                votes: 1,
+              })
+            );
+          });
+      });
+      test("Status: 200 - accept an object of inc_votes which decrements vote by a negative number", () => {
+        const negativeVote = { inc_votes: -100 };
+        return request(app)
+          .patch(`/api/articles/4`)
+          .send(negativeVote)
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.article).toEqual(
+              expect.objectContaining({
+                article_id: 4,
+                title: expect.any(String),
+                topic: "mitch",
+                author: "rogersop",
+                body: expect.any(String),
+                created_at: expect.any(String),
+                votes: -100,
+              })
+            );
+          });
+      });
+      test("status: 400 - reponse with err msg for invalid inc_vote request", () => {
+        const invalidVote = { inc_votes: NaN };
+        return request(app)
+          .patch("/api/articles/3")
+          .send(invalidVote)
+          .expect(400)
+          .then(({ body: { msg } }) => {
+            expect(msg).toBe("Bad Request");
+          });
+      });
+      test("status: 400 - reponse with err msg inc_vote spelt wrong but with VALID number", () => {
+        const invalidVote = { inc_voes: 3 };
+        return request(app)
+          .patch("/api/articles/3")
+          .send(invalidVote)
+          .expect(400)
+          .then(({ body: { msg } }) => {
+            expect(msg).toBe("Bad Request");
+          });
+      });
+      test("status: 400 - reponse with err msg when extra key is added to the inc_vote request", () => {
+        const invalidVote = { inc_votes: 3, title: "invalid" };
+        return request(app)
+          .patch("/api/articles/3")
+          .send(invalidVote)
+          .expect(400)
+          .then(({ body: { msg } }) => {
+            expect(msg).toBe("Bad Request");
+          });
+      });
     });
   });
-});
+
+  
 
 describe("/api/users", () => {
   describe("GET", () => {
