26d50e10cfe860cc589e557677e8bbfa870819df
Claire Farrow <clairefarrow.cp@gmail.com>
2022-02-25 10:59:00 +0000

    debugging for heroku

--- db/connection.js
+++ db/connection.js
@@ -6,14 +6,17 @@ require('dotenv').config({
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE not set');
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const config = ENV === 'production' ? {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-} : {};
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
 module.exports = new Pool(config);
